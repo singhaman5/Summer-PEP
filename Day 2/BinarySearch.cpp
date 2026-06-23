f467aed8e9da5e6d63898bea39f8b5bc38840b05
Aman Singh <amansinghx04@gmail.com>
2026-06-23 11:53:47 +0530

    Updated

---
+++ Day 2/BinarySearch.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5] = {2,4,5,7,9};
+    int n = (sizeof(arr)/sizeof(arr[0]));
+    int target = 5;
+    int lo=0, hi=n-1;
+    int result = -1;
+    while(lo<=hi){
+        int mid = lo+(hi-lo)/2;
+        if(arr[mid]==target){
+            result = mid;
+            break;
+        }
+        else if(arr[mid] < target){
+            lo = mid+1;
+        }
+        else hi = mid-1;
+    }
+    cout<<result<<endl;
+}
--- Day 2/MoveZeroesToEnd.cpp
+++ Day 2/MoveZeroesToEnd.cpp
@@ -17,6 +17,7 @@ int main(){
     }
 }
 
+
 // Using count of Zeroes
 
 #include<iostream>
