3a672dc00fe71edd47b247cf21289a65f683e3b7
Aman Singh <amansinghx04@gmail.com>
2026-06-23 10:05:39 +0530

    Updated

--- Day 2/ReverseAnArray.cpp
+++ Day 2/ReverseAnArray.cpp
@@ -1,22 +1,22 @@
 // // Two Pointer Approach
 
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int arr[5] = {1,2,3,4,5};
-//     int n = (sizeof(arr)/sizeof(arr[0]));
-//     int left = 0, right = n-1;
-//     while(left<right){
-//         int temp = arr[left];
-//         arr[left] = arr[right];
-//         arr[right] = temp;
-//         left++;
-//         right--;
-//     }
-//     for(int i=0; i<n ;i++){
-//         cout<<arr[i]<<" ";
-//     }
-// }
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    int n = (sizeof(arr)/sizeof(arr[0]));
+    int left = 0, right = n-1;
+    while(left<right){
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--; 
+    }
+    for(int i=0; i<n ;i++){
+        cout<<arr[i]<<" ";
+    }
+}
 
 
 // Using Stack
