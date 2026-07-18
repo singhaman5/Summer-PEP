63cbd2ba28e1ad53593e9099a8ccdd943d35d6ab
Aman Singh <amansinghx04@gmail.com>
2026-07-18 17:05:35 +0530

    Updated

---
+++ Day 18/QueueUsingArray.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+class Queue{
+public:
+    int f; // Front
+    int b; // Back
+    int arr[5];
+    Queue(){
+        f = 0;
+        b = 0;
+    }
+    void push(int val){
+        if(b==5){
+            cout<<"Queue is full"<<endl;
+            return;
+        }
+        arr[b] = val;
+        b++;
+    }
+    void pop(){
+        if(f-b==0){ // (f-b) gives size
+            cout<<"Queue is EMPTY!"<<endl;
+        }
+        f++;
+    }
+    int front(){
+        if(f-b==0){ // (f-b) gives size
+            cout<<"Queue is EMPTY!"<<endl;
+            return -1;
+        }
+        return arr[f];
+    }
+    int back(){
+        if(f-b==0){ // (f-b) gives size
+            cout<<"Queue is EMPTY!"<<endl;
+            return -1;
+        }
+        return arr[b-1];
+    }
+    int size(){
+        return f-b;
+    }
+    bool isEmpty(){
+        if(b-f==0) return true;
+        else return false;
+    }
+    void display(){
+        for(int i=f;i<b;i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+
+};
+int main(){
+    Queue q;
+    // Push, pop, front->top, size(empty), back
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.display();
+    q.push(50);
+    q.push(60);
+    q.display();
+    q.pop();
+    q.display();
+}
